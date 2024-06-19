f6a1f85f36e78bd5776d505f9d2b0df7674388fb
josfam <jamukun@proton.me>
2024-06-20 00:58:53 +0300

    Fix: reload the db storage on module import

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,10 @@
-"""Database models"""
+#!/usr/bin/env/ python3
+
+"""For defining table models, and the storage engine"""
+
+import os
+from .engine.db import DB
+
+storage = DB()
+
+storage.reload()
