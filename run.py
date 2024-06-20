20efdad8af1fd7d8732211375616f3c0b513074b
josfam <jamukun@proton.me>
2024-06-20 14:39:16 +0300

    Run a flask app, serving placeholder content

--- run.py
+++ run.py
@@ -4,3 +4,15 @@
 
 from flask import Flask
 from app.models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """The home screen that is shown"""
+    return 'Welcome to spendwise!'
+
+
+if __name__ == '__main__':
+    app.run()
