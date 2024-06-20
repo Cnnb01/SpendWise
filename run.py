dd850400cba01b154b8e1cf73672d3fb512f6f84
josfam <jamukun@proton.me>
2024-06-20 15:02:18 +0300

    Serve the signup page as the homepage

--- run.py
+++ run.py
@@ -2,17 +2,19 @@
 
 """Runs the spendwise application"""
 
-from flask import Flask
+from flask import Flask, render_template
 from app.models import storage
 
-app = Flask(__name__)
+app = Flask(
+    __name__, template_folder='app/templates', static_folder='app/static'
+)
 
 
 @app.route('/', strict_slashes=False)
 def home():
-    """The home screen that is shown"""
-    return 'Welcome to spendwise!'
+    """Shows the homepage of the application"""
+    return render_template('signup.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
