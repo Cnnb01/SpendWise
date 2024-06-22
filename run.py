25062a26ec32c9675ef0117220b00bc2f34ce13d
josfam <jamukun@proton.me>
2024-06-22 19:32:04 +0300

    Fix: Specify location of templates and static
    The `templates` and `static` directories are now recognized by flask

--- run.py
+++ run.py
@@ -3,11 +3,17 @@
 """Runs the spendwise application"""
 
 from flask import Flask, render_template
+from spendwise.api.v1.auth import auth_bp
 from spendwise.models import storage
 
 app = Flask(
-    __name__, template_folder='spendwise/templates', static_folder='spendwise/static'
+    __name__,
+    template_folder='spendwise/templates',
+    static_folder='spendwise/static',
 )
+
+# register blueprints
+app.register_blueprint(auth_bp, url_prefix='/api/v1')
 
 
 @app.route('/', strict_slashes=False)
