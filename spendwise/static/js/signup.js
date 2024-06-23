69c1eebf9a9b31377969b64e3e37f7f0cd5ac057
josfam <jamukun@proton.me>
2024-06-23 17:47:12 +0300

    Add alert messages for sucessful/failed signup

--- spendwise/static/js/signup.js
+++ spendwise/static/js/signup.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('#signup-form').on('submit', function(event) {
+  $('#signup-form').on('submit', function (event) {
     event.preventDefault(); // prevent default form submission behavior
 
     const firstName = $('#first-name').val();
@@ -8,17 +8,28 @@ $(document).ready(function () {
     const passwd = $('#passwd').val();
     const confirmedPasswd = $('#confirm-passwd').val();
 
-    if (passwd != confirmedPasswd) {
+    if (passwd !== confirmedPasswd) {
       alert('Passwords don\'t match. Try again');
       return;
     }
+
+    const showSignupSuccess = function (response) {
+      alert(response.message);
+    };
+
+    const showSignupFailure = function (jqXHR, textStatus, errorThrown) {
+      const message = jqXHR.responseJSON.message;
+      alert(message);
+    };
 
     // AJAX request to send the form
     $.ajax({
       url: '/api/v1/register',
       type: 'POST',
       contentType: 'application/json',
-      data: JSON.stringify({firstName: firstName, lastName: lastName, email: email, passwd: passwd}),
+      data: JSON.stringify({ firstName, lastName, email, passwd }),
+      success: showSignupSuccess,
+      error: showSignupFailure
     });
   });
 });
