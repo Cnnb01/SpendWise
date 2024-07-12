8e40c0f1f327e335771ac3f7b9b693a152394086
josfam <jamukun@proton.me>
2024-07-12 09:32:35 +0300

    Make rendered date human-readable

--- spendwise/static/js/expense_display.js
+++ spendwise/static/js/expense_display.js
@@ -5,17 +5,26 @@ $(document).ready(function() {
       success: function(response) {
         // item name, category, amount spent, date added
         response.forEach(expense => {
-          const expenseRow = $(`<tr></tr>`)
+          const expenseRow = $(`<tr></tr>`);
 
+          // format the date to be human-readable
+          const rawDate = new Date(expense.dateAdded);
+          const dateOptions = {
+            weekday: 'short', // e.g., Thu
+            day: 'numeric',   // e.g., 4
+            month: 'short',   // e.g., Jun
+            year: 'numeric'   // e.g., 2024
+          };
+          const formattedDate = rawDate.toLocaleDateString(undefined, dateOptions);
           // add columns to the row
           expenseRow.append(`<td>${expense.itemName}</td>`);
           expenseRow.append(`<td>${expense.categoryName}</td>`);
           expenseRow.append(`<td>${expense.expenseAmount}</td>`);
-          expenseRow.append(`<td>${expense.dateAdded}</td>`);
+          expenseRow.append(`<td>${formattedDate}</td>`);
 
           $('#expense_table').append(expenseRow);
         });
-        console.log(response);
+        
       },
       error: function(error) {
           console.log("Error:", error);
