ae747128b49df10f2be49a00539af2deaab69d35
josfam <jamukun@proton.me>
2024-07-14 09:02:00 +0300

    Use template strings for building categories table

--- spendwise/static/js/category_display.js
+++ spendwise/static/js/category_display.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
     function loadCategories(budgetId){
         $.ajax({
-            url: "/api/v1/budgets/" + budgetId + "/categories",
+            url: `/api/v1/budgets/${budgetId}/categories`,
             type: "GET",
             success: function(data){
                 console.log("Received data:", data);//debbugging
                 $("#category_table").find("tr:gt(0)").remove();//clearing existing rows except the header
                 $("h1").text(data.budgetTitle + " Categories");
                 data.categories.forEach(function(category){
-                    $('#category_table').append(
-                        '<tr>' +
-                        '<td>' + category.category_name + '</td>' +
-                        '<td>' + category.amount_budgeted + '</td>' +
-                        '</tr>'
-                    );
+                  const category_table = $('#category_table');
+                  const table_row = $(`<tr></tr>`)
+                  table_row.append(`<td>${category.category_name}</td>`)
+                  table_row.append(`<td>${category.amount_budgeted}</td>`)
+                  category_table.append(table_row);
                 });
             },
             error: function(error){
@@ -23,8 +22,8 @@ $(document).ready(function(){
     }
 
     // extract budgetId from URL path
-    var pathArray = window.location.pathname.split('/');
-    var budgetId = pathArray[pathArray.length - 2];
+    const pathArray = window.location.pathname.split('/');
+    const budgetId = pathArray[pathArray.length - 2];
 
     if (budgetId) {
         loadCategories(budgetId);
